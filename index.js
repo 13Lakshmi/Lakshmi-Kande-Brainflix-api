2647a8c6b350ee847837e56372bc99ac6fec8cc0
Lakshmi <kandelakshmi13@gmail.com>
2024-03-08 17:09:43 -0700

    modified get

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-
-
+const cors = require("cors");
 const videosRoutes = require("./routes/videosRoutes");
 
 const PORT = 8080;
 
 app.use(express.json());
-
+app.use(express.static("public"));
+app.use(cors());
 
 app.use("/videos",videosRoutes);
 
-// app.get("/",(req,res)=>{
-//     res.send("Welcome");
-// });
+
 
 
 app.listen(PORT,()=>{
--- routes/videosRoutes.js
+++ routes/videosRoutes.js
@@ -6,51 +6,54 @@ const { v4: uuid } = require("uuid");
 
 
 router.get("/",(req,res) => {
-    const videosJSON = fs.readFileSync("./data/video-details.json")
-    const videos = JSON.parse(videosJSON);
-    console.log(videos);
+    const videosBuffer = fs.readFileSync("./data/video-details.json")
+    const videos = JSON.parse(videosBuffer);
     res.status(201).send(videos);
 })
 
 
 router.get("/:videosId",(req,res) =>{
     const { videosId } = req.params;
-    const videosJSon = fs.readFileSync("./data/video-details.json");
-    const videos = JSON.parse(videosJSon);
+    
+    const videosBuffer = fs.readFileSync("./data/video-details.json");
+    const videos = JSON.parse(videosBuffer);
 
-    const selectedVideos = videos.find(
-        (videodetail) => videodetail.id === videosId
+   // console.log(videos);
+
+    const selectedVideo = videos.find(
+        (videolist) => videolist.id === videosId
     );
-    res.status(210).send(selectedVideos);
+    res.status(201).send(selectedVideo);
 });
+
+
+
 
 // router.post("/",(req,res) =>{
 //    console.log(req.body);
-
 //     res.send("vidoes submitted");
 // })
 
 
-// router.post("/",(req,res) =>{
-//     console.log(req.body);
+router.post("/",(req,res) =>{
+    console.log(req.body);
 
+    const videosJSON = fs.readFileSync("./data/video-details.json");
+    const videos = JSON.parse(videosJSON);
 
-//     const videosJSON = fs.readFileSync("./data/video-details.json");
-//     const videos = JSON.parse(videosJSON);
+    const newVideoDetail = {
+        id : uuid(),
+        title: req.body.title,
+        channel:req.body.channel,
+    };
 
-//     const newVideoDetail = {
-//         id : uuid(),
-//         title: req.body.title,
-//         channel:req.body.channel,
-//     };
+    videos.push(newVideoDetail);
 
-//     videos.push(newVideoDetail);
+    const videosSTRINGIFIED = JSON.stringify(videos);
+    fs.writeFileSync("./data/video-details.json",videosSTRINGIFIED);
 
-//     const videosSTRINGIFIED = JSON.stringify(videos);
-//     fs.writeFileSync("./data/video-details.json",videosSTRINGIFIED);
-
-//     res.status(201).send("videos submitted successfully")
-// });
+    res.status(201).send("videos submitted successfully")
+});
 
 
 
